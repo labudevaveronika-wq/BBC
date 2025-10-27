81d160572ed7510a14dda4c4da27e8b4011365f1
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-10-27 22:20:27 +0300

    сохранение комнаты с ключом после посещения

--- homework3/main.py
+++ homework3/main.py
@@ -2,34 +2,36 @@ import random
 from random import randint
 
 n, m = int(input()), int(input())
+n += 2
+m += 2
 pospx = 1
 pospy = 1
 posex = randint(2, n-2)
 posey = randint(2, m-2)
-keyx = [i for i in range(2, n) if i != posex]
-keyy = [i for i in range(2, n) if i != posex]
+keyx = [i for i in range(2, n-2) if i != posex]
+keyy = [i for i in range(2, n-2) if i != posex]
 key_flag = False
 poskx = random.choice(keyx)
 posky = random.choice(keyy)
 
 massive_for_player = []
-for i in range(n+1):
+for i in range(n+2):
     row = []
-    for j in range(m+1):
-        if i == 0 or j == m or i == n or j == 0: row.append('.')
+    for j in range(m+2):
+        if i == 0 or j == m-1 or i == n-1 or j == 0: row.append('.')
         else: row.append('#')
     massive_for_player.append(row)
 
 
 hidden_massive = []
-for i in range(n+1):
+for i in range(n+2):
     row = []
-    for j in range(m+1):
-        if i == 0 or j == m or i == n or j == 0: row.append('.')
+    for j in range(m+2):
+        if i == 0 or j == m-1 or i == n-1 or j == 0: row.append('.')
         else: row.append('#')
     hidden_massive.append(row)
-for i in range(n + 1):
-    for j in range(m + 1):
+for i in range(n):
+    for j in range(m):
         if i == pospy and j == pospx: hidden_massive[i][j] = "P"
         elif i == posey and j == posex: hidden_massive[i][j] = "E"
         elif i == posky and j == poskx: hidden_massive[i][j] = "K"
@@ -38,25 +40,30 @@ for i in range(n + 1):
 
 while (pospx != posex or pospy != posey) or key_flag == False:
     massive_for_player[pospy][pospx] = hidden_massive[pospy][pospx]
-    for i in range(n+1):
-        for j in range(m+1):
+    for i in range(n):
+        for j in range(m):
             if i == pospy and j == pospx: print("P", end= " ")
             else: print(massive_for_player[i][j], end=" ")
         print()
 
     act = str(input())
-    massive_for_player[pospy][pospx] = "#"
+    if not (pospx == poskx and pospy == posky or pospx == posex and pospy == posey):
+        massive_for_player[pospy][pospx] = "#"
     new_px, new_py = pospx, pospy
 
     if act == 'w': new_py -= 1
     elif act == 's': new_py += 1
     elif act == 'd': new_px += 1
     elif act == 'a': new_px -= 1
-    if new_px == poskx and new_py == posky: key_flag = True
+    if new_px == poskx and new_py == posky:
+#        print("!!!")
+        key_flag = True
+        print("Ты нашел(ла) ключ, надо дойти до портала!")
+        input("Нажми Enter чтобы продолжить...")
+        massive_for_player[posky][poskx] = "K"
 
-    if 1 <= new_px <= m and 1 <= new_py <= n: pospx, pospy = new_px, new_py
+    if 1 <= new_px < m-1 and 1 <= new_py < n-1: pospx, pospy = new_px, new_py
     else: print("Нельзя совершить такое действие")
-    massive_for_player[pospy][pospx] = "P"
 print("Вы выиграли!")
 # w - вверх
 # a - влево
