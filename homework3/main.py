5f82dc4857dcaf8308b5a8195448b9cba6944ca7
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-10-27 21:05:11 +0300

    написала концепцию нахождения ключа и перехода в портал

--- homework3/main.py
+++ homework3/main.py
@@ -7,9 +7,10 @@ posex = randint(2, n-2)
 posey = randint(2, m-2)
 keyx = [i for i in range(2, n) if i != posex]
 keyy = [i for i in range(2, n) if i != posex]
+key_flag = False
 poskx = random.choice(keyx)
 posky = random.choice(keyy)
-while pospx != posex or pospy != posey:
+while (pospx != posex or pospy != posey) or key_flag == False:
     for i in range(n+1):
         for j in range(m+1):
             if i == pospy and j == pospx: print("P", end= " ")
@@ -24,9 +25,10 @@ while pospx != posex or pospy != posey:
     elif act == 's': new_py += 1
     elif act == 'd': new_px += 1
     elif act == 'a': new_px -= 1
+    if new_px == poskx and new_py == posky: key_flag = True
 
     if 1 <= new_px <= m and 1 <= new_py <= n: pospx, pospy = new_px, new_py
-    else: print("Нельзя соврешить такое действие")
+    else: print("Нельзя совершить такое действие")
 print("Вы выиграли!")
 # w - вверх
 # a - влево
