0b95ac18943e64ff63f72f4b4bd51dd1f3d5401d
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-10-27 19:52:01 +0300

    Добавила первое действие игрока

--- homework3/main.py
+++ homework3/main.py
@@ -1,9 +1,27 @@
-posx = 1
-posy = 1
+from random import randint
 n, m = int(input()), int(input())
-for i in range(n+1):
-    for j in range(m+1):
-        if i == 1 and j == 1: print("P", end= " ")
-        elif i == 0 or j == m  or i == n or j == 0: print('.', end=' ')
-        else: print("*", end=" ")
-    print()
+pospx = 1
+pospy = 1
+posex = randint(2, n-1)
+posey = randint(2, m-1)
+while pospx != posex and pospy != posey:
+    for i in range(n+1):
+        for j in range(m+1):
+            if i == pospy and j == pospx: print("P", end= " ")
+            elif i == posey and j == posex: print("E", end= " ")
+            elif i == 0 or j == m  or i == n or j == 0: print('.', end=' ')
+            else: print("*", end=" ")
+        print()
+    act = str(input())
+    new_px, new_py = pospx, pospy
+    if act == 'w': new_py -= 1
+    elif act == 's': new_py += 1
+    elif act == 'd': new_px += 1
+    elif act == 'a': new_px -= 1
+
+    if 0 <= new_px <= m and 0 <= new_py <= n: pospx, pospy = new_px, new_py
+    else: print("Нельзя соврешить такое действие")
+# w - вверх
+# a - влево
+# s - вниз
+# d - вправо
