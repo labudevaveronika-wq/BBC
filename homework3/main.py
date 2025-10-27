0fd478681d5c4dce94b00426dba77fffb0104a93
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-10-27 20:56:34 +0300

    создала ключ

--- homework3/main.py
+++ homework3/main.py
@@ -1,14 +1,20 @@
+import random
 from random import randint
 n, m = int(input()), int(input())
 pospx = 1
 pospy = 1
 posex = randint(2, n-2)
 posey = randint(2, m-2)
+keyx = [i for i in range(2, n) if i != posex]
+keyy = [i for i in range(2, n) if i != posex]
+poskx = random.choice(keyx)
+posky = random.choice(keyy)
 while pospx != posex or pospy != posey:
     for i in range(n+1):
         for j in range(m+1):
             if i == pospy and j == pospx: print("P", end= " ")
             elif i == posey and j == posex: print("E", end= " ")
+            elif i == posky and j == poskx: print("K", end=" ")
             elif i == 0 or j == m  or i == n or j == 0: print('.', end=' ')
             else: print("*", end=" ")
         print()
