96299df24658b4421979524d6a787de0917d33f4
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-11-20 07:50:32 +0300

    Что-то попыталсь добавить, что помню и что поняла как делать

--- seminar_4/main.py
+++ seminar_4/main.py
@@ -12,15 +12,14 @@
 #import numpy as np
 
 
-
 import pandas as pd
 import numpy
 df = pd.read_csv('tested.csv')
 
-#print("Кол-во пропусков: ", df.isna().sum().sum())
-#print(df.describe())
+print("Кол-во пропусков: ", df.isna().sum().sum())
+#print(df.describe()) # характеристики
 #print(df.tail(6))
-#print(df.info())
+#print(df.info()) # какая инфомрмация в каком столбце
 #print(df.head(6))
 #print(df.shape) '''(418, 12)'''
 #print(df.columns) '''Index(['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], dtype='object')'''
@@ -31,3 +30,22 @@ df = pd.read_csv('tested.csv')
 #print(df.isnull()) '''возвращает DataFrame с булевыми значениями (True, где пропуск).'''
 #print(df.dropna()) ''' удаляет строки или столбцы с пропущенными значениями'''
 #print(df.fillna(0)) '''заполняет пропущенные значения указанным значением'''
+
+import pandas as pd
+import numpy as np
+
+# 1
+d = pd.read_csv("tested.csv")
+print(d.describe())
+print(d.isnull().sum())
+print(d.info())
+print(f"{len(d)} - колличество строк, {len(d.columns)} - колличество столбцов")
+# sred = d["Age"].sum() / len(d["Age"])
+print(d["Age"].mean(), "- средний возраст")
+d.fillna({"Age": d["Age"].mean()}, inplace=True)  # пересоздание и сохранение ланных в таблице df
+print(d["Age"]) # вывели посмотреть что стало
+d = d.dropna(subset=["Embarked"])  # удаляет строки в кторых нет пункта назначения гостя
+
+# 2
+
+print(d["Survived"].mean() * 100) # % выживших
